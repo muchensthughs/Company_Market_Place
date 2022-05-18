29fc4b4aac7cd85ade06167bc7f21e88d2d1599e
Evan Yang <evan.yang@sisu.edu>
2022-05-18 03:29:05 -0700

    update sql

--- SQLScripts/products.sql
+++ SQLScripts/products.sql
@@ -11,5 +11,14 @@ VALUES
 ('Sharon Mccoy', 'hellopika', 'Very good at communicating and presenting.', 'https://hellopika.com/cookdetail.php?id=bf7b0502-d482-11ec-9d64-0242ac120002'),
 ('Alayna Conrad', 'hellopika', 'The process of cooking is always very interesting to me.', 'https://hellopika.com/cookdetail.php?id=c63af56e-d482-11ec-9d64-0242ac120002'),
 ('Annie Ayala', 'hellopika', 'You will love what I cook.', 'https://hellopika.com/cookdetail.php?id=cd8d6784-d482-11ec-9d64-0242ac120002'),
-('Mary Land', 'hellopika', 'Love cooking Italian cuisine', 'https://hellopika.com/cookdetail.php?id=f1e5de00-d481-11ec-9d64-0242ac120002')
-;
+('Mary Land', 'hellopika', 'Love cooking Italian cuisine', 'https://hellopika.com/cookdetail.php?id=f1e5de00-d481-11ec-9d64-0242ac120002'),
+('Art Class', 'My Cram School', 'Art Foundations A/B is a lively, inviting, comprehensive course written for beginning level artists.', 'https://tsyang.com/homework1/products/Art.php'),
+('Biology Class', 'My Cram School', 'This course begins with a consideration of the living condition and discussion of the unique properties of living organisms that set life apart from the non-living.', 'https://tsyang.com/homework1/products/Biology.php'),
+('Chemistry Class', 'My Cram School', 'CHEM:1070 provides students with an introduction to chemistry and is appropriate for students who have not had an advanced chemistry course in high school.', 'https://tsyang.com/homework1/products/Chemistry.php'),
+('Chinese Class', 'My Cram School', 'Chinese 102 is a beginning level course in Modern Standard Chinese (Mandarin).', 'https://tsyang.com/homework1/products/Chinese.php'),
+('English Class', 'My Cram School', 'This course has been designed to give you the fundamental knowledge you will need to be successful in high school-level English classes, and to pass the California High School Exit Exam (CAHSEE).', 'https://tsyang.com/homework1/products/English.php'),
+('Japanese Class', 'My Cram School', 'Elementary Japanese A is an introduction to Japanese language and culture, and it is designed for students who have had little or no experience learning Japanese.', 'https://tsyang.com/homework1/products/Japanese.php'),
+('Law Class', 'My Cram School', 'Traditionally, property was a six-hour course taught over the entire first year of law school.', 'https://tsyang.com/homework1/products/Law.php'),
+('Math Class', 'My Cram School', 'The curriculum for this course aligns with the College and Career Readiness Standards for Mathematics', 'https://tsyang.com/homework1/products/Math.php'),
+('Physics Class', 'My Cram School', 'Physics is an advanced level science class that satisfies the high school graduation requirement of a physical science class.', 'https://tsyang.com/homework1/products/Physics.php'),
+('Program Class', 'My Cram School', 'This course is designed as an introduction to programming and programming language Python for the students who have no or very little programming knowledge and experience.', 'https://tsyang.com/homework1/products/Program.php'); 
